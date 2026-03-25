9862f835d41adbb89c32a58efb5c07ce1e0c5806
Miabread <miabreadloaf@gmail.com>
2026-03-25 13:39:22 -0400

    Change date display to be relative

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,8 +8,27 @@ export function cn(...inputs: ClassValue[]) {
 
 export const uppercaseFirstLetter = (input: string) => input.charAt(0).toUpperCase() + input.slice(1);
 
-export const formatDate = (date: number | Date) =>
-    Intl.DateTimeFormat(undefined, { dateStyle: 'short', timeStyle: 'short' }).format(date);
+const timeFormat = new Intl.RelativeTimeFormat('en', { numeric: 'always' });
+
+const unitCutoffs = [
+    [31536000, 'years'],
+    [2592000, 'months'],
+    [86400, 'days'],
+    [3600, 'hours'],
+    [60, 'minutes'],
+    [0, 'seconds'],
+] as const;
+
+// I hate js why do I gotta implement this myself
+export const formatDate = (date: number) => {
+    const seconds = Math.floor((Date.now() - date) / 1000);
+
+    for (const [cutoff, unit] of unitCutoffs) {
+        if (seconds >= cutoff) {
+            return timeFormat.format(-Math.floor(seconds / cutoff), unit);
+        }
+    }
+};
 
 export const readFileAsText = (file: File): Promise<string> =>
     new Promise((resolve, reject) => {

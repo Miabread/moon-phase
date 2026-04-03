d04ee82f6d39de901cb2ffaa5bba7c69e9f834ba
Miabread <miabreadloaf@gmail.com>
2026-04-02 22:30:06 -0400

    Improve EasterEgg :3

--- src/components/custom/EasterEgg.vue
+++ src/components/custom/EasterEgg.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { Rabbit } from 'lucide-vue-next';
+import Tooltip from './Tooltip.vue';
+import type { HTMLAttributes } from 'vue';
+
+const props = defineProps<{ style?: HTMLAttributes['style'] }>();
 </script>
 
 <template>
-    <Rabbit class="hover:animate-[bounce_250ms_infinite]" />
+    <Tooltip title="🥕" as-child>
+        <Rabbit v-bind="props" class="hover:animate-[bounce_250ms_infinite]" />
+    </Tooltip>
 </template>

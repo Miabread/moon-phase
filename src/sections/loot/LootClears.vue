26fd02f620cd5eace1488e19bb6a76775e00e856
Miabread <miabreadloaf@gmail.com>
2026-04-17 14:19:07 -0400

    Start work on loot clears

--- src/sections/loot/LootClears.vue
+++ src/sections/loot/LootClears.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/shadcn/table';
+import { Tooltip } from '@/components/shadcn/tooltip';
+import { LOOT } from './loot';
+</script>
+
 <template>
-    <div class="grid place-items-center">Work in Progress</div>
+    <Table>
+        <TableBody>
+            <TableRow v-for="lootSet in LOOT" :key="lootSet.title">
+                <TableHead>
+                    {{ lootSet.title }}
+                </TableHead>
+                <TableCell v-for="loot in lootSet.loot" :key="loot.title">
+                    <Tooltip :title="loot.title" class="flex justify-center items-center">
+                        <img :src="loot.icon" :alt="loot.title" class="h-10 w-10" />
+                    </Tooltip>
+                </TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
 </template>
